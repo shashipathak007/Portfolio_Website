f7931a89e4111c2d261017af94353082559111c7
shashipathak007 <shashipathak446@gmail.com>
2026-01-13 12:08:19 +0545

    navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { useEffect, useState } from "react"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Navbar from "./components/Navbar"
 
 const App = () => {
+  const [darkMode, setDarkMode]=useState(true)
+  useEffect(()=> {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      offset: 100
+    });
+  })
   return (
     <div>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Navbar />
     </div>
   )
 }
